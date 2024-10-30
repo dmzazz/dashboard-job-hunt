59c1a514bfc347b8190f5573899d274cf4ab8919
Dimas Azizir <dimaslaylaa@gmail.com>
2024-10-30 20:21:28 +0700

    integration signin

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,10 @@ export const hashPassword = async (password: string) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   return hashedPassword;
 };
+
+// Compare password
+export const comparePassword = async (password: string, hashedPassword: string) => {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+
+  return isMatch;
+};
