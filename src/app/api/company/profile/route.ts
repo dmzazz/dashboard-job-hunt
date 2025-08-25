e8d99085f443ccc425123694fff25bbd7ad753f3
Dimas Azizir <dimaslaylaa@gmail.com>
2025-08-25 20:21:06 +0700

    update: get session

--- src/app/api/company/profile/route.ts
+++ src/app/api/company/profile/route.ts
@@ -1,10 +1,25 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../../lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
+    // Get session
+    const session = await getServerSession(authOptions);
+
+    // Check if email exist
+    if (!session?.user.email) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // Retrieve data based on the users email
     const result = await prisma.company.findMany({
-      include: {
+      where: { email: session.user.email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
         CompanyOverview: true,
         CompanySocialMedia: true,
         CompanyTeam: true,
