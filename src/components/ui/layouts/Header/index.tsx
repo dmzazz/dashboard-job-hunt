eba188e0f54010b06c792cd2f0a9c31a48edb032
Dimas Azizir <dimaslaylaa@gmail.com>
2025-08-26 17:47:45 +0700

    update: add status loading for session user

--- src/components/ui/layouts/Header/index.tsx
+++ src/components/ui/layouts/Header/index.tsx
@@ -1,27 +1,32 @@
 "use client";
 
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Button } from "../../button";
 import { PlusIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { Skeleton } from "../../skeleton";
 
 interface HeaderProps {}
 
 export const Header: FC<HeaderProps> = ({}) => {
   const router = useRouter();
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   const navCreateJobPAge = () => router.push("/post-a-job");
   return (
-    <div className="pb-3 mb-8 border-b border-border flex flex-row items-center justify-between">
+    <div className="mb-8 flex flex-row items-center justify-between border-b border-border pb-3">
       <div>
         <div>Company</div>
-        <div className="font-semibold">{session?.user.name}</div>
+        {status === "loading" ? (
+          <Skeleton className="h-6 w-36" />
+        ) : (
+          <div className="font-semibold">{session?.user.name}</div>
+        )}
       </div>
-      <div className="rounded-none py-3 px-6">
+      <div className="rounded-none px-6 py-3">
         <Button onClick={navCreateJobPAge}>
-          <PlusIcon className="mr-2 2-4 h-4" />
+          <PlusIcon className="2-4 mr-2 h-4" />
           Post a job
         </Button>
       </div>
