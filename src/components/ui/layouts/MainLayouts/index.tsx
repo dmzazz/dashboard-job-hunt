4dc803ce5a9c83067c2a90070d99a658cb2a139a
Dimas Azizir <dimaslaylaa@gmail.com>
2025-08-31 15:24:34 +0700

    add: overlay effect to main content

--- src/components/ui/layouts/MainLayouts/index.tsx
+++ src/components/ui/layouts/MainLayouts/index.tsx
@@ -9,21 +9,26 @@ const MainLayouts = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const [isOpen, setIsOpen] = useState<boolean>(true);
-
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <div className="border-t">
       <div className="bg-background">
-        <div className="flex flex-row">
-          <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+
+        {/* Overlay Main Content */}
+        {isOpen && (
           <div
-            className={`col-span-3 min-h-screen overflow-auto lg:col-span-5 ${isOpen ? "ml-[280px] flex-grow" : "ml-[80px] flex-grow"} transition-all`}
-          >
-            <div className="px-6 py-6 lg:px-8">
-              <Header />
-              {children}
-            </div>
+            className="fixed inset-0 z-40 bg-black/50"
+            onClick={() => setIsOpen(false)}
+          />
+        )}
+
+        {/* Main Content */}
+        <div className="col-span-3 ml-[80px] min-h-screen lg:col-span-5">
+          <div className="px-6 py-6 lg:px-8">
+            <Header />
+            {children}
           </div>
         </div>
       </div>
